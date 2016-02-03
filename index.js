85c19b81fc1b6da4c11bc619a61adcdfc2702171
Arata Tanaka <tarata43@yahoo.co.jp>
2016-02-03 20:09:08 +0900

    Add logger error

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ Path = require('path');
 
 module.exports = function(robot) {
     process.env.HUBOT_ATTEND_MANAGER_CREDENTIAL || robot.logger.error("Environment Variable 'HUBOT_ATTEND_MANAGER_CREDENTIAL' is required");
+    process.env.HUBOT_ATTEND_MANAGER_CALENDARID || robot.logger.error("Environment Variable 'HUBOT_ATTEND_MANAGER_CALENDARID' is required");
     var path = Path.resolve(__dirname, 'src/hubot');
     Fs.exists(path, function(exists) {
         if (exists) {
